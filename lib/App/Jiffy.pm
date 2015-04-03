7c3d5082e2cb4050ed8943f716f0e59a8e1970fe
Sean Zellmer <sean@lejeunerenard.com>
2015-04-02 22:17:59 -0500

    Add simple App::Jiffy object

--- lib/App/Jiffy.pm
+++ lib/App/Jiffy.pm
@@ -1,8 +1,52 @@
 package App::Jiffy;
 
 use strict;
+use warnings;
+
 use 5.008_005;
 our $VERSION = '0.01';
+
+use App::Jiffy::TimeEntry;
+use YAML::Any qw( LoadFile );
+
+use Moo;
+
+has cfg => (
+  is  => 'ro',
+  default => sub {
+    LoadFile($ENV{HOME} . '/.jiffy.yml') || {};
+  },
+);
+
+=head2 add_entry
+
+C<add_entry> will create a new TimeEntry with the current time as the entry's start_time.
+
+=cut
+
+sub add_entry {
+  my $self = shift;
+  my $title = shift;
+
+  # Create and save Entry
+  App::Jiffy::TimeEntry->new(
+    title => $title,
+    cfg => $self->cfg,
+  )->save;
+}
+
+=head2 run
+
+C<run> will start an instance of the Jiffy app.
+
+=cut
+
+sub run {
+  my $self = shift;
+  my @args = @_;
+
+  $self->add_entry(join ' ' , @_);
+}
 
 1;
 __END__
