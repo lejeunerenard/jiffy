d81f50080ed328afeaf19a9744f8b8137706cb21
Sean Zellmer <sean@lejeunerenard.com>
2015-04-02 22:17:05 -0500

    Complete basic TimeEntry - save(), cfg(), db() a and Class find()
    I plan to proxy TimeEntry::find() on App::Jiffy as find_entry().

--- lib/App/Jiffy/TimeEntry.pm
+++ lib/App/Jiffy/TimeEntry.pm
@@ -3,8 +3,11 @@ package App::Jiffy::TimeEntry;
 use strict;
 use warnings;
 
-use Moo;
 use Scalar::Util qw( blessed );
+use MongoDB;
+use MongoDB::OID;
+
+use Moo;
 
 has id => (
    is    => 'rw',
@@ -13,20 +16,36 @@ has id => (
    },
 );
 
-
 has start_time => (
    is  => 'rw',
    isa => sub {
       die 'start_time is not a DateTime object' unless blessed $_[0] and $_[0]->isa('DateTime');
    },
+   default => sub {
+     DateTime->now();
+   },
 );
 
 has title => (
    is  => 'rw',
-   isa => sub {
-   },
    required => 1,
 );
+
+has cfg => (
+  is => 'ro',
+);
+
+=head2 db
+
+C<db> get the db handler from MongoDB
+
+=cut
+
+sub db {
+  my $self = shift;
+  my $client = MongoDB::MongoClient->new;
+  return $client->get_database( $self->cfg->{db} || 'jiffy' );
+}
 
 =head2 save
 
@@ -37,12 +56,39 @@ C<save> will commit the current state of the C<TimeEntry> to the database.
 sub save {
    my $self = shift;
    my $_id = $self->id;
+   my $document = {
+     start_time => $self->start_time,
+     title =>$self->title,
+   };
    if ( $_id ) {
-      # Update the existing record
+     # Update the existing record
+     $self->db->get_collection('timeEntry')->update( { _id => $self->id  }, { '$set' => $document  }  );
    } else {
-      # Insert new record
+     # Insert new record
+     $_id = $self->db->get_collection('timeEntry')->insert($document);
+     # Update id
+     $self->id($_id);
    }
 }
-1;
 
-__END__
+=head2 find
+
+C<find> will return a single document. The query will use the provided C<_id>.
+
+=cut
+
+sub find {
+  my $cfg = shift;
+  my $_id = shift;
+
+  my $client = MongoDB::MongoClient->new;
+  my $entry = $client->get_database( $cfg->{db} )->get_collection('timeEntry')->find_one({ _id => $_id });
+  return unless $entry;
+  return App::Jiffy::TimeEntry->new(
+    id => $entry->{_id},
+    title => $entry->{title},
+    start_time => $entry->{start_time},
+    cfg => $cfg,
+  );
+}
+1;
